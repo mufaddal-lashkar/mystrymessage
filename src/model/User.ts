2c7ef1623badfa7da72194fa0b2d8cf9bd586f93
mufaddal lashkar <mufaddal.github@gmail.com>
2024-07-19 12:09:47 +0530

    models and zod schema done

---
+++ src/model/User.ts
@@ -0,0 +1,74 @@
+import mongoose, {Schema, Document} from "mongoose"; // using document for typescript
+
+// message schema starts
+
+export interface Message extends Document{
+    content: string;
+    createdAt: Date
+} // extends document cuz its part of mongoose
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+// user schema starts
+
+export interface User extends Document{
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isVerified: boolean;
+    isAcceptingMessages: boolean;
+    messages: Message[]
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match: [/.+\@.+\..+/, "Please use a valid email"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "Verify Code is required"]
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verify Code Expiry is required"]
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAcceptingMessages: {
+        type: Boolean,
+        default: true
+    },
+    messages: [MessageSchema]
+})
+
+// we have to check if we alr have model or not bcz next js runs on edge computer so if we have model alr use it else create a model
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+
+export default UserModel;
