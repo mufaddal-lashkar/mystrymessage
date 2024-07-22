c805c39a27078b3d9a5602b8e07f0ccfd17d36ae
mufaddal lashkar <mufaddal.github@gmail.com>
2024-07-22 16:45:01 +0530

    sign up frontend done

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -5,7 +5,7 @@ import * as z from "zod"
 import Link from "next/link"
 import axios, { AxiosError } from "axios"
 import React,{ useEffect, useState } from "react"
-import { useDebounceValue } from 'usehooks-ts'
+import { useDebounceCallback } from 'usehooks-ts'
 import { useToast } from "@/components/ui/use-toast"
 import { useRouter } from "next/navigation"
 import { signUpSchema } from "@/schemas/signupSchema"
@@ -18,11 +18,12 @@ import { Divide, Loader2 } from "lucide-react"
 const Page = () => {
   const [username, setUsername] = useState('')
   const [usernameMessage, setUsernameMessage] = useState('')
+  // const [isUsernameAvailable, setIsUsernameAvailable] = useState(false)
   const [isCheckingUsername, setIsCheckingUsername] = useState(false)
   const [isSubmitting, setIsSubmitting] = useState(false)
   // const [isSubmitted, setIsSubmitted] = useState(false)
   
-  const debouncedUsername = useDebounceValue(username, 300)
+  const debounced = useDebounceCallback(setUsername, 300)
   const { toast } = useToast()
   const router = useRouter()
 
@@ -39,22 +40,25 @@ const Page = () => {
   // check username is available
   useEffect(() => {
     const checkUsernameUnique = async () => {
-      if(debouncedUsername) {
+      if(username) {
         setIsCheckingUsername(true)
         setUsernameMessage("")
+        // setIsUsernameAvailable(false)
         try {
-          const response = await axios.get(`/api/check-username-unique?username=${debouncedUsername}`)
+          const response = await axios.get(`/api/check-username-unique?username=${username}`)
           setUsernameMessage(response.data.message)
+          // setIsUsernameAvailable(true)
         } catch (error) {
           const axiosError = error as AxiosError<ApiResponse>
           setUsernameMessage(axiosError.response?.data.message ?? "Error while checking username")
+          // setIsUsernameAvailable(false)
         } finally {
           setIsCheckingUsername(false)
         }
       }
     }
     checkUsernameUnique()
-  },[debouncedUsername])
+  },[username])
 
   // submit handler
   const onSubmit = async (data:z.infer<typeof signUpSchema>) => {
@@ -98,10 +102,12 @@ const Page = () => {
                     <FormControl>
                       <Input placeholder="username" {...field} onChange={(e) => {
                         field.onChange(e)
-                        setUsername(e.target.value)
+                        debounced(e.target.value)
                       }}/>
                     </FormControl>
                     {isCheckingUsername && <><div className="flex items-center"><Loader2 className="animate-spin h-4 w-4"/><p className="text-xs">Checking for available username</p></div></>}
+                    {/* {isUsernameAvailable ? <p className="text-green-700">Username is available</p> : <p className="text-red-500">Username is not available</p>} */}
+                    <p className={`text-sm ${usernameMessage === "Username is available" ? 'text-green-500' : 'text-red-500'}`}>{usernameMessage}</p>
                   <FormMessage />
                 </FormItem>
               )}
