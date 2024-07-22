68cd619110673040acde364212a4d0e0e3fed824
mufaddal lashkar <mufaddal.github@gmail.com>
2024-07-22 17:28:50 +0530

    verify frontend done

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -10,7 +10,7 @@ export async function sendVerificationEmail(
     try {
         await resend.emails.send({
             from: 'onboarding@resend.dev',
-            to: email,
+            to: "mufaddal.lashkar@gmail.com",
             subject: 'Mystry Message | Verification Code',
             react: VerificationEmail({username, otp: verifyCode})
           });
